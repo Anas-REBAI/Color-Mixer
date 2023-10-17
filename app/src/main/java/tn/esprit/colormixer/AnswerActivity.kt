0d5bb700e7137725bdf1be2e44e3f0642e6c0108
Anas <anas.rebai@esprit.tn>
2023-10-18 00:12:23 +0100

    finished (Activity + intent)

---
+++ app/src/main/java/tn/esprit/colormixer/AnswerActivity.kt
@@ -0,0 +1,71 @@
+package tn.esprit.colormixer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import tn.esprit.colormixer.databinding.ActivityAnswerBinding
+
+class AnswerActivity : AppCompatActivity() {
+
+    //TODO 12 Add lateint var for binding
+    private lateinit var binding: ActivityAnswerBinding
+
+    private var correctColor = "NONE"
+    private var name = "NONE"
+    private var color1 = "NONE"
+    private var color2 = "NONE"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //TODO 13 Bind the view and implement setContentView()
+        binding = ActivityAnswerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //TODO 14 Change the value of correctColor / name / color1 / color2 with the DATA from the INTENT
+        correctColor = intent.getStringExtra(MIXED_COLOR).toString()
+        name = intent.getStringExtra(NAME).toString()
+        color1 = intent.getStringExtra(COLOR1).toString()
+        color2 = intent.getStringExtra(COLOR2).toString()
+
+        //TODO 15 Change the txtChoosed with : "You chose $color1 and $color2"
+        binding.txtChoosed.text = "you choose $color1 and $color2 "
+
+        //TODO 16 Implement setOnClickListener on the btnSubmit and call checkAnswer()
+        // You must check if only one radio button is selected,
+        // the navigate to the ResultActivity with the data name and RESULT (FAILED/SUCCESS)
+        binding.btnSubmit.setOnClickListener(){
+            if (!binding.rbGreen.isChecked && !binding.rbOrange.isChecked && !binding.rbPurple.isChecked){
+                Snackbar.make(binding.contextView, "you must choose a color to answer", Snackbar.LENGTH_SHORT).show()
+                //sortir de la gestion de clic, car il n'est pas nécessaire de poursuivre si l'utilisateur n'a pas fait de sélection
+                return@setOnClickListener
+            }
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra(NAME, name)
+                putExtra(RESULT, FAILED)
+            }
+            if (checkAnswer()){
+                intent.apply { putExtra(RESULT, SUCCESS) }
+            }
+            startActivity(intent)
+
+            // finish() : fermer l'activité actuelle après avoir démarré l'activité ResultActivity.
+            // Cela peut être utile pour libérer des ressources et nettoyer l'activité actuelle.
+            finish()
+        }
+
+    }
+
+    private fun checkAnswer(): Boolean{
+        //TODO 17 Check if the answer of the chosen color is correct
+        if (binding.rbGreen.isChecked && correctColor.equals(GREEN)){
+            return true
+        }else if (binding.rbOrange.isChecked && correctColor.equals(ORANGE)){
+            return true
+        }else if (binding.rbPurple.isChecked && correctColor.equals(PURPLE)){
+            return true
+        }
+            return false
+    }
+}
